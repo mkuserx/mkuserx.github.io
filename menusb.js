45ace9a2d3fb8cd78ba625108b44d19500300a38
mkuserx <yhoelh00@gmail.com>
2025-10-31 18:58:00 -0500

    Update menusb.js

--- menusb.js
+++ menusb.js
@@ -1,4 +1,4 @@
-/*
+
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
@@ -6,7 +6,7 @@
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional*/
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   /*const firebaseConfig = {
     apiKey: "AIzaSyCtao-ISry3kKY7PoOPdzpbvVLk1lg9SAA",
     authDomain: "mkuserx-article.firebaseapp.com",
@@ -16,9 +16,8 @@
     messagingSenderId: "349673286607",
     appId: "1:349673286607:web:a1a2a98b826e6903f640ba",
     measurementId: "G-ZSWQ29544J"
-  };
-*/
-  /* Initialize Firebase
+  };*/
+  /*Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);*/
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
