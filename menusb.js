595ed8afd9f19c1fe95e2670a1ede977dc2bc7da
mkuserx <yhoelh00@gmail.com>
2025-10-31 18:31:49 -0500

    Update menusb.js

--- menusb.js
+++ menusb.js
@@ -7,7 +7,7 @@
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional*/
-  const firebaseConfig = {
+  /*const firebaseConfig = {
     apiKey: "AIzaSyCtao-ISry3kKY7PoOPdzpbvVLk1lg9SAA",
     authDomain: "mkuserx-article.firebaseapp.com",
     databaseURL: "https://mkuserx-article-default-rtdb.firebaseio.com",
@@ -17,7 +17,18 @@
     appId: "1:349673286607:web:a1a2a98b826e6903f640ba",
     measurementId: "G-ZSWQ29544J"
   };
-
+*/
   /* Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);*/
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCtao-ISry3kKY7PoOPdzpbvVLk1lg9SAA",
+  authDomain: "mkuserx-article.firebaseapp.com",
+  databaseURL: "https://mkuserx-article-default-rtdb.firebaseio.com",
+  projectId: "mkuserx-article",
+  storageBucket: "mkuserx-article.firebasestorage.app",
+  messagingSenderId: "349673286607",
+  appId: "1:349673286607:web:a1a2a98b826e6903f640ba",
+  measurementId: "G-ZSWQ29544J"
+};
